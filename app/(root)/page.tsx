b7aa3e3698d150f5e0e3a89479679177dc0e3f61
Aayush <aayush.perecharla@infodatinc.com>
2023-12-11 08:42:05 -0600

    temp

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import { UserButton } from "@clerk/nextjs";
 import { useEffect } from "react";
 
 const SetupPage = () => {
+  /*
     const onOpen = useStoreModal((state) => state.onOpen);
     const isOpen = useStoreModal((state) => state.isOpen);
 
@@ -13,7 +14,7 @@ const SetupPage = () => {
         onOpen()
       }
     }, [isOpen, onOpen])
-
+  */
     return (
       <div className='p-4'>
         <UserButton></UserButton>
