9245076e7d2a3530db1c599102904faea0b0456b
Aayush <aayush.perecharla@infodatinc.com>
2023-12-11 08:24:50 -0600

    added store modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs'
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect (() => {
+      if(!isOpen) {
+        onOpen()
+      }
+    }, [isOpen, onOpen])
+
     return (
       <div className='p-4'>
-        <UserButton/>
+        <UserButton></UserButton>
       </div>
     )
   }
