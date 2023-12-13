a80256eda399d72673eb13659ddddb2f5d7d7672
Aayush <aayush.perecharla@infodatinc.com>
2023-12-13 01:04:22 -0600

    Updated hook

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -16,6 +16,7 @@ import axios from "axios"
 import { useParams, useRouter } from "next/navigation"
 import { AlertModal } from "@/components/modals/alert-modal"
 import { ApiAlert } from "@/components/ui/api-alert"
+import { useOrigin } from "@/hooks/use-origin"
 
 interface SettingsFormProps {
     initialData: Store
@@ -31,6 +32,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 }) => {
     const params = useParams()
     const router = useRouter()
+    const origin = useOrigin()
     const[open, setOpen] = useState(false)
     const[loading, setLoading] = useState(false)
 
