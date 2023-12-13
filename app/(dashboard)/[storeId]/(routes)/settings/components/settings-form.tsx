77051c131850c7d0d71aeaeedfc3fc7a6eaf9a26
Aayush <aayush.perecharla@infodatinc.com>
2023-12-13 01:01:50 -0600

    Added admin API alerts

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -15,6 +15,7 @@ import { Input } from "@/components/ui/input"
 import axios from "axios"
 import { useParams, useRouter } from "next/navigation"
 import { AlertModal } from "@/components/modals/alert-modal"
+import { ApiAlert } from "@/components/ui/api-alert"
 
 interface SettingsFormProps {
     initialData: Store
@@ -113,6 +114,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
                     </Button>
                 </form>
             </Form>
+            <Separator/>
+            <ApiAlert
+                title="NEXT_PUBLIC_API_URL"
+                description= {`${origin}/api/${params.storeId}`}
+                variant= "public"
+            />
         </>
     )
 }
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -12,6 +12,12 @@ export function MainNav ({
 
     const routes = [
         {
+            href: `/${params.storeId}`,
+            label: 'Home',
+            active: pathname === `/${params.storeId}`
+        },
+
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}`
