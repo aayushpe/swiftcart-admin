0effc1126eb2f3bdb2247b74d62c6549d92bc57b
Aayush <aayush.perecharla@infodatinc.com>
2023-12-14 02:20:36 -0600

    Added login button from landing page

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
 
     return(
-      <p>Landing page!!</p>
+      <div>
+        <p className="ml-2">Landing Page!</p>
+        <br></br>
+        <Button className="ml-2" asChild>
+          <Link href="/sign-in">Login</Link>
+        </Button>
+      </div>
       
     )
   }
