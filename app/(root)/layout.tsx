de86675c28ea92f4c345c82aefa059ec9ed30e1b
aayushpe <aayushpe@gmail.com>
2023-12-20 23:47:39 -0600

    changed redirect function to landing page to async

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,7 @@ export default async function SetupLayout ({
     const { userId } = auth()
 
     if (!userId) {
-        redirect('/landing')
+        await redirect('/landing')
     }
 
     const store = await prismadb.store.findFirst({
